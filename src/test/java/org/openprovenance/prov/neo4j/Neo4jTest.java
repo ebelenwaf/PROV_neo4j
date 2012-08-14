42a19932ec0cd5796c7ff5c9e556a647d223edce
weishi423 <w.shi2@ncl.ac.uk>
2012-08-14 11:53:55 +0100

    first commit

---
+++ src/test/java/org/openprovenance/prov/neo4j/Neo4jTest.java
@@ -0,0 +1,19 @@
+package org.openprovenance.prov.neo4j;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class Neo4jTest extends TestCase
+{
+    public void testNeo4j() throws  java.lang.Throwable {
+
+    	Neo4jUtility u = new Neo4jUtility();
+
+    	u.asn2neo4j("../asn/src/test/resources/prov/w3c-publication3.prov-asn",null);
+	
+    }
+}
+
+
+
+                  
